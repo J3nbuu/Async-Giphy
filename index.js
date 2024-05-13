52754cecab0e61794f1663a677bdca2a88f59da2
J3nbuu <jenbu3d@gmail.com>
2024-05-13 16:50:06 +0100

    added api and code

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 require('dotenv').config();
+//const fetch = require('node-fetch');
+//
+const endpoint = 'https://api.giphy.com/v1/gifs/search?api_key=EsOQGTMeFpZz3DbZvbWiNE8QBSCH8zm1&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips';
+async function getImage(query){;
+        const response = await fetch(endpoint + "&q=" + query);
+        const data = await response.json();
+        const gifUrl = data.data[0].url
+        console.log("gif Url:", gifUrl)
+        return gifUrl
+}
+
+
+getImage('cat')
+
+
+
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
